bac8daa8c9e1a83b3052145f938f4210b9609c49
creative-crew98 <crewcreative98@gmail.com>
2026-08-11 02:12:07 +0530

    Medusa

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -9,16 +9,12 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
-  storeCors: process.env.STORE_CORS!,
-  adminCors: process.env.ADMIN_CORS!,
-  authCors: process.env.AUTH_CORS!,
-  jwtSecret: process.env.JWT_SECRET,
-  cookieSecret: process.env.COOKIE_SECRET,
-  sessionOptions: {
-    sameSite: "none",
-    secure: true,
-  },
-},
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET,
+      cookieSecret: process.env.COOKIE_SECRET,
+    },
   },
   admin: {
     disable: false,
